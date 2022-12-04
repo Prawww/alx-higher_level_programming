9e2228b9bdb7359fafa6317a94a98680625f0676
anamkara <philipnyoro4@gmail.com>
2022-12-04 05:34:16 -0800

    importing modules

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first
+        b: second
+
+    Returns:
+        The returns value. a + b
+    """
+    return (a +b)
