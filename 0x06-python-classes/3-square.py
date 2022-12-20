5654eade35b43f72d09cb42e57dc4c76aecda997
anamkara <philipnyoro4@gmail.com>
2022-12-20 13:58:07 -0800

    python classes

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,11 +6,11 @@ class Square():
     """square class with it's size and proper validation"""
 
     def __init__(self, size=0)
-    if (type(size) is not int):
-        raise TypeError("size must be an integer")
-    elif (size < 0):
-        raise ValueError("size must be >= 0")
-    self.__size = size
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+            self.__size = size
 
     def area(self):
         return self.__size ** 2
