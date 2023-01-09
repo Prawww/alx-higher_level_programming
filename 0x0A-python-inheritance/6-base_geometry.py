a3d1281f8c35fbe32dcd090b2c3cd9c809ea44e1
anamkara <philipnyoro4@gmail.com>
2023-01-09 11:47:39 -0800

    python ingeritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" Defines a class BaseGeometry """
+
+
+class BaseGeometry:
+    """class represents base geometyr """
+    def area(self):
+        """public instance method """
+        raise Exception("area() is not implemented")
