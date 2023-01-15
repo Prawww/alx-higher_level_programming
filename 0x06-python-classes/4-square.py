67865ede82990d8ea724d8fbe5ec37a8c69bf256
anamkara <philipnyoro4@gmail.com>
2023-01-15 04:36:35 -0800

    python classes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,7 +6,7 @@ class Square():
     """square class with it's size and proper validation"""
 
     def __init__(self, size=0):
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
