672b6bda4e6cb7ee87ff6302843c73c7e87801d0
anamkara <philipnyoro4@gmail.com>
2023-01-15 06:01:24 -0800

    python classes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -15,11 +15,6 @@ class Square():
             """"get size"""
             return self.__size
 
-        @property
-        def position(self):
-            """"get position"""
-            return self.__position
-
         @size.setter
         def size(self, value):
             """"set size"""
@@ -28,6 +23,11 @@ class Square():
             elif (value < 0):
                 raise ValueError("size must be >= 0")
             self.__size = value
+        
+        @property
+        def position(self):
+            """get position"""
+            return self.__position
 
         @position.setter
         def position(self, value):
@@ -53,6 +53,6 @@ class Square():
             else:
                 for i in range(self.__position[1]):
                     print("")
-                for i in range(self.__size):
-                    print(" " * self.__position[0], end="")
-                    print("#" * self.__size)
+                    for i in range(self.__size):
+                        print(" " * self.__position[0], end="")
+                        print("#" * self.__size)
