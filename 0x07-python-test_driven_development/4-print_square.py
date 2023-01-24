bdacebf1e3640ef0e4cec71d3e30ac7528d7c033
anamkara <philipnyoro4@gmail.com>
2023-01-24 09:30:46 -0800

    test driven dev

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -11,7 +11,7 @@ def print_square(size):
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if (size < 0):
-         raise ValueError("size must be >= 0")
+        raise ValueError("size must be >= 0")
     if size == 0:
         return
     for i in range(size):
