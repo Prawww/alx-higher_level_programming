f0f8b21ab0f8d8ebd893bc144a76e0be353ec7be
anamkara <philipnyoro4@gmail.com>
2023-03-13 21:40:55 -0700

    javascript basics

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 const {argv} = require('process');
-if (argv.length === 2)
+if (argv.length >= 2)
 {
-	console.log("No argument");
-}
-else if (argv.length === 3)
-{
-	console.log('Argument found');
-}
-else
-{
+	if (argv.length === 2)
+		console.log("No argument");
+
+	else if (argv.length === 3)
+		console.log('Argument found');
+
 	console.log('Arguments found');
 }
