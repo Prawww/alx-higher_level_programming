5a381ab0628010ac7bc317680a2ec145fc0a3275
anamkara <philipnyoro4@gmail.com>
2023-03-14 11:50:59 -0700

    js advanced

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,13 +1,12 @@
-#!usr/bin/node
+#!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) { [this.weight, this.height] = [w, h]; }
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
-
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.weight));
+      console.log('X'.repeat(this.width));
     }
   }
 };
