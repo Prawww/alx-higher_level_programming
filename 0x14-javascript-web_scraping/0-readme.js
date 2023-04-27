ca5be69182b3ea1bc34da3fe85c407a2c6b3cabc
root <philipnyoro4@gmail.com>
2023-04-27 12:02:16 +0000

    web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(content);
-  }
+  console.log(error || content);
 });
 
