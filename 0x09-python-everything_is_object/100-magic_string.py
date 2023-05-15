e30ed1ea4dea1749c89f09f25ff0eb1f906ad30f
root <philipnyoro4@gmail.com>
2023-05-15 15:52:19 +0000

    debugging

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def magic_string(l=[]):
-    """function to print a magic string"""
-
+    """function to print a magic string
+    Args:
+	l[] (list): the list
+	"""
     l += ["BestSchool"]
     return ", ".join(l)
